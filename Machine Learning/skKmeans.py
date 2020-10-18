b2c1094e8815d2182f0d7a95259094540a8e2e4c
deveshreddy <deveshreddy27@gmail.com>
2020-10-18 18:31:45 -0500

    Hybrid models dont work

---
+++ Machine Learning/skKmeans.py
@@ -0,0 +1,18 @@
+from sklearn.cluster import KMeans
+
+
+
+
+class skKmeans():
+    def __init__(self):
+        
+
+    def trainer():
+
+
+    def transformer(self, model, data)
+        
+
+# Take the data, create a trainer
+# Save the trainer, and transform train and test data
+# Deal with the returned labels in the main function.
